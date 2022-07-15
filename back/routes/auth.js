ac42d0a17dc0a41cd3abeffb6b19621e2983b04d
IdanHamo <97957689+IdanHamo@users.noreply.github.com>
2022-07-15 11:55:32 +0300

    .

---
+++ back/routes/auth.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
+const { User } = require("../model/users");
+
+router.post("/", async (req, res) => {
+  const { error } = validation(req.body);
+
+  if (error) return res.status(400).send(error);
+  const user = await User.findOne({ email: req.body.email });
+  if(!user)
+});
+
+function validation(values) {
+  const schema = Joi.object({
+    email: Joi.string().min(6).max(400).email().required(),
+    password: Joi.string().min(6).max(1024).required(),
+  });
+
+  return schema.validate(values);
+}
